129f494c25045ecd63aa64b8157d243da1da3569
biotin <biotin@debian.debian>
2014-01-29 18:20:59 +0100

    SimpleTestApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Employees::Application.config.secret_key_base = 'dd17f9ab9d0975fdf95b2c2e47054511c9c7d5493dabc4b13578498542919647ac5f1dea2e2f29a539ce33b04685d5771f802a6dd401197e6b8de6753e58701b'
